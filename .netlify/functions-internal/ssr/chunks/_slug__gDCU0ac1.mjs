63365e8c25d535799dce25c42bfbe2add2ac3ccd
dkeithdj <42316655+dkeithdj@users.noreply.github.com>
2023-12-21 22:11:21 +0800

    clean

--- .netlify/functions-internal/ssr/chunks/_slug__gDCU0ac1.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/_slug__woMon-y8.mjs').then(n => n._);
-
-export { page };
